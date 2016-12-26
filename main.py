c2171ded145b0c73e0710cef9a89a60848cb471d
bhaveshAn <bhaveshanand7@gmail.com>
2016-12-26 15:29:47 +0530

    Increasing the functionality

--- main.py
+++ main.py
@@ -2,15 +2,25 @@ import kivy
 kivy.require('1.8.0')
 from kivy.app import App 
 from kivy.uix.gridlayout import GridLayout 
-
+from math import *
+from kivy.uix.widget import Widget
 class CalcGrid(GridLayout):
+	mode="radians"
+	def angles(self):
+		if mode=="radians":
+			mode="degrees"
+		else:
+			mode="radians"
+
+
+
+
 	def calculate(self,calculation):
 		if calculation:
 			try:
 				self.display.text=str(eval(calculation))
 			except:
 				self.display.text="Error"
-				
 
 class CalculatorApp(App):
 	def build(self):
